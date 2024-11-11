678abddeaed6f87631f2e1a8b964f47f6e5bd8ed
VitaliyMenyaylo <92362737+VitaliyMenyaylo@users.noreply.github.com>
2024-11-11 18:37:51 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,11 @@
+from operator import truediv
+
+name = "Vitaliy"
+print ("name: ",name)
+age = 44
+print ('age: ', age)
+age = age + 1
+print ('age: ', age)
+is_student = True
+print ('is_student: ', is_student)
+
