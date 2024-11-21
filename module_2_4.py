45ea6a3547403566e6af151bae9a216f0bd29fcf
VitaliyMenyaylo <92362737+VitaliyMenyaylo@users.noreply.github.com>
2024-11-21 18:18:57 +0300

    Update module_2_4.py

--- module_2_4.py
+++ module_2_4.py
@@ -6,12 +6,12 @@ for i in numbers :
     if (i > 0) and (i < 3) : #отсеем отрицательные числа и 0, если они попали по ошибке в список
         primes.append(i)
     elif i > 2 :
-        Primes_number = True
+        is_prime = True
         for j in range(2, i - 1) :
             if i % j == 0 :
-                Primes_number = False
+                is_prime = False
                 break
-        if Primes_number :
+        if is_prime :
             primes.append(i)
         else :
             not_primes.append(i)
