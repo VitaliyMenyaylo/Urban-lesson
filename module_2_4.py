9a141f4c73dce4c2e2e9271692c8ccd9b0325ccb
VitaliyMenyaylo <92362737+VitaliyMenyaylo@users.noreply.github.com>
2024-11-21 18:13:32 +0300

    Add files via upload

---
+++ module_2_4.py
@@ -0,0 +1,18 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+# может быть любая последовательность, в любом порядке...
+primes = []
+not_primes = []
+for i in numbers :
+    if (i > 0) and (i < 3) : #отсеем отрицательные числа и 0, если они попали по ошибке в список
+        primes.append(i)
+    elif i > 2 :
+        Primes_number = True
+        for j in range(2, i - 1) :
+            if i % j == 0 :
+                Primes_number = False
+        if Primes_number :
+            primes.append(i)
+        else :
+            not_primes.append(i)
+print ("простые числа",primes)
+print("не простые числа",not_primes)
